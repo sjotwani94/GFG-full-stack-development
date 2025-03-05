7bfe71e52d83d14f580b4eb8054b849e01bf4ce3
sjotwani94 <siddharthkj345@gmail.com>
2025-03-06 01:06:11 +0530

    Add Check And Delete Todo Functionalities, Minor Enhancements

--- Todo-List/app.js
+++ Todo-List/app.js
@@ -2,9 +2,6 @@ const form = document.querySelector('#form');
 const list = document.querySelector('#list');
 const input = document.getElementById('todo-input');
 
-// Strikethrough when checked
-// Delete Functionality
-
 class TodoList {
     constructor(todos) {
         this.todos = todos;
@@ -23,12 +20,30 @@ class TodoList {
         }
         for (let todo of this.todos) {
             const li = document.createElement('li');
+            const label = document.createElement('label');
+            label.innerHTML = todo.task;
+
             const checkbox = document.createElement('input');
             checkbox.setAttribute('type', 'checkbox');
             checkbox.setAttribute('class', 'form-check-input');
-        
-            li.innerText = todo.task + ' ';
-            li.append(checkbox);
+            checkbox.checked = todo.completed;
+            checkbox.addEventListener('click', () => {
+                this.checkTodo(todo.id);
+            })
+
+            const trashIcon = document.createElement('i');
+            trashIcon.setAttribute('class', 'bi bi-trash');
+            trashIcon.addEventListener('click', () => {
+                this.deleteTodo(todo.id);
+                li.remove();
+            });
+
+            const container = document.createElement('span');
+            container.append(checkbox);
+            container.append(label);
+
+            li.append(container);
+            li.append(trashIcon);
             list.append(li);
         }
     }
@@ -37,6 +52,24 @@ class TodoList {
         this.todos.push({ id: TodoList.uuid(), task: todoText, completed: false });
         localStorage.setItem('TodoList', JSON.stringify(this.todos));
         this.refreshTodos();
+    }
+
+    deleteTodo(todoId) {
+        this.todos = this.todos.filter((todo) => todo.id !== todoId);
+        localStorage.setItem('TodoList', JSON.stringify(this.todos));
+    }
+
+    checkTodo(todoId) {
+        this.todos = this.todos.reduce((accumulator, todo) => {
+            if (todo.id === todoId) {
+                const editedTodo = { ...todo, completed: !todo.completed };
+                accumulator.push(editedTodo);
+            } else {
+                accumulator.push(todo);
+            }
+            return accumulator;
+        }, []);
+        localStorage.setItem('TodoList', JSON.stringify(this.todos));
     }
 
     init() {
